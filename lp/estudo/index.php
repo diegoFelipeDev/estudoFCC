54c6ed9d66a41e41756bfd903ea9f7550f84bdd8
Diego Felipe <161398995+diegoFelipeDev@users.noreply.github.com>
2024-03-14 22:37:56 -0300

    Create index.php

---
+++ lp/estudo/index.php
@@ -0,0 +1,36 @@
+<?php  //echo"Hello Word";
+
+//String
+
+/*
+$mensagem = 'hello word';
+	echo $mensagem;
+*/
+
+//Operadores
+
+/*
+$A = 2;
+$B = 3;
+
+$soma = $A + $B;
+	print $soma; 
+*/
+
+//Estruturas condicionais
+
+/*
+$A = 5;
+$B = 2;
+
+if ($B % 2 == 1){
+	echo 'Número Ímpar';
+}
+
+else{
+	echo 'Número Par';
+}
+*/
+
+//Laços de repetição
+
